80e064462de8b7d4cc1463a6441505a35f54e586
LucianoMO101 <luciano.mollenochoa@gmail.com>
2025-09-29 09:27:27 +0200

    claude versie updated

--- client.py
+++ client.py
@@ -65,7 +65,7 @@ class MCPClient:
 
         # Initial Claude API call
         response = self.anthropic.messages.create(
-            model="claude-3-5-sonnet-20241022",
+            model="claude-3-5-haiku-20241022",
             max_tokens=1000,
             messages=messages,
             tools=available_tools
